cb4403a0c950d9110733919e740fc266457dae7c
troy66 <94767797+serhii66@users.noreply.github.com>
2026-03-06 19:46:25 +0100

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T18:46:20.803Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T18:46:25.083Z
+Commit: Add export functionality for reports
