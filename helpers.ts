3cecfa95fa0315a0948847f7ffac73697803524a
troy66 <94767797+serhii66@users.noreply.github.com>
2026-03-06 19:46:16 +0100

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T18:46:14.830Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T18:46:16.463Z
+Commit: Patch security vulnerability in authentication
