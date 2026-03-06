c0024c4621296cbab9fb2accc011b03fbeb972d4
troy66 <94767797+serhii66@users.noreply.github.com>
2026-03-06 19:46:21 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T18:46:01.037Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T18:46:20.803Z
+Commit: Fix CSS styling bug on mobile devices
