7473f640b23b834a3f328dff232ff06ed6568931
troy66 <94767797+serhii66@users.noreply.github.com>
2026-03-09 08:04:36 +0100

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T07:04:27.631Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-09T07:04:36.269Z
+Commit: Fix CSS styling bug on mobile devices
