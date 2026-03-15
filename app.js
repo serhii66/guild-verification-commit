623b4046db2439441d72c85fe31afddd1e76c6cd
troy66 <94767797+serhii66@users.noreply.github.com>
2026-03-15 10:58:50 +0100

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T07:04:37.496Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-15T09:58:49.594Z
+Commit: Fix CSS styling bug on mobile devices
