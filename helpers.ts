4ceb6af4b0e25337c9d740f8199a3c0859e255ae
troy66 <94767797+serhii66@users.noreply.github.com>
2026-03-15 10:58:58 +0100

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-09T07:04:36.269Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-15T09:58:57.073Z
+Commit: Introduce real-time notifications
